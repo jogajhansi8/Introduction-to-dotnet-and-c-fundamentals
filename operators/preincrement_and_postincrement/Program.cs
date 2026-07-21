9253245497bff7a83766bb6c9010f76edbbe2022
jogajhansi8 <jogajhansi8@gmail.com>
2026-07-21 15:30:56 +0530

    Add files via upload

---
+++ operators/preincrement_and_postincrement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1, num2;
+
+        Console.Write("Enter num1: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter num2: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        int a = num1;
+        int b = ++a;
+        Console.WriteLine("\nAfter Pre-Increment:");
+        Console.WriteLine("num1 = " + a);
+        Console.WriteLine("num2 = " + b);
+        a = num1;
+        b = a++;
+        Console.WriteLine("\nAfter Post-Increment:");
+        Console.WriteLine("num1 = " + a);
+        Console.WriteLine("num2 = " + b);
+
+        Console.WriteLine("\nBefore Swapping:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("\nAfter Swapping:");
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+    }
+}
